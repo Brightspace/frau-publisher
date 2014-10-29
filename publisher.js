ddecff6524c054e926d1b23c1416729b3ce8bbfe
Eugene Zhang <Eugene.Zhang@d2l.com>
2014-10-29 14:03:56 -0400

    Separated creds check into it's own function, and only allow key and secret to be in the aws.

--- publisher.js
+++ publisher.js
@@ -3,23 +3,8 @@
 var s3 = require('gulp-s3');
 
 module.exports = function( opts ) {
-	var aws;
-	if (!opts || !opts.creds ) {
-		throw new Error('Invalid arguments');
-	} else if (typeof opts.creds == 'string') {
-		try {
-			aws = require(opts.creds);
-		} catch (err) {
-			throw new Error( 'Invalid creds location' );
-		}
-	} else if ( !opts.creds.key || !opts.creds.secret ) {
-		throw new Error('Invalid arguments');
-	} else {
-		aws = opts.creds;
-	}
-
-	if ( !opts.appID )
-		throw new Error('Invalid arguments');
+	
+	var aws = checkValidity( opts );
 
 	aws.bucket = 'gaudi-cdn-test';
 
@@ -30,3 +15,34 @@ module.exports = function( opts ) {
 
 	return s3( aws, options );
 };
+
+var checkValidity = function ( opts ) {
+	var aws;
+
+	if (!opts || !opts.creds ) {
+		throw new Error('Invalid arguments');
+	} else if (typeof opts.creds == 'string') {
+		try {
+			var tempAws = require(opts.creds);
+			aws = {
+				key: tempAws.key,
+				secret: tempAws.secret
+			}
+		} catch (err) {
+			throw new Error( 'Invalid creds location' );
+		}
+	} else if ( !opts.creds.key || !opts.creds.secret ) {
+		throw new Error('Invalid arguments');
+	} else {
+		aws = {
+			key: opts.creds.key,
+			secret: opts.creds.secret
+		};
+	}
+
+	if ( !opts.appID )
+		throw new Error('Invalid arguments');
+
+	return aws;
+	
+} 
