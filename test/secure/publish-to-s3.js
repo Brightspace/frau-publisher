09289a67f564034cd2d5b712479adb935305ea8e
Carl Pacey <Carl.Pacey@d2l.com>
2014-11-04 17:34:11 -0500

    Adding an integration test to ensure that a published file is readable

---
+++ test/secure/publish-to-s3.js
@@ -0,0 +1,50 @@
+var frauPublisher = require('../../publisher'),
+	request = require('request'),
+	eventStream = require('event-stream'),
+	gUtil = require('gulp-util');
+
+var publisher = frauPublisher({
+	appID: 'frau-publisher-test',
+	creds: {
+		key: '***REMOVED***',
+		secret: process.env.CREDS_SECRET
+	},
+	devTag: 'test'
+});
+
+var content = 'some data';
+var filename = 'test' + Math.random().toString(16).slice(2) + '.txt';
+var file = new gUtil.File({
+	cwd: '/',
+	base: '/',
+	path: '/' + filename,
+	contents: new Buffer(content),
+	stat: {
+		size: content.length
+	}
+});
+
+describe('publisher', function() {
+	it('should publish new file', function(cb) {
+		console.log('file: ' + filename);
+
+		eventStream.readArray( [file] )
+			.pipe( publisher )
+			.on('end', function() {
+				// gulp-s3 sends the end event before it's actually done, so we need to introduce an artificial wait.
+				setTimeout(function() {
+					request.get( publisher.location + filename, function(error, result, body) {
+						if (error) {
+							cb(error);
+						} else if (result.statusCode != 200 ) {
+							cb(result.statusCode);
+						} else if ( body !== content ){
+							cb(body);
+						} else {
+							cb();
+						}
+					});
+				}, 500);
+			});
+	});
+});
