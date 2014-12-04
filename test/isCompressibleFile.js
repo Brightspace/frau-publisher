019c1cc4a0b643b7a2e36e4c1755bf5e1b67dd50
Dave Lockhart <dave.lockhart@desire2learn.com>
2014-12-04 13:03:44 -0500

    Simplifying isCompressibleFile tests using forEach

--- test/isCompressibleFile.js
+++ test/isCompressibleFile.js
@@ -3,39 +3,31 @@ var fs = require('fs'),
 	._isCompressibleFile,
 	path = require('path');
 
-function isCompressibleHelper( filename ) {
-	var filePath = path.join( './test/support', filename );
-	var stream = fs.createReadStream( filePath );
-	var isCompressible = isCompressibleFile( stream );
-	return isCompressible;
-}
+var compressibleFiles = [
+	'file.js',
+	'file.css',
+ 	'file.html',
+	'uppercase.HTML'
+];
 
 describe( 'compressor', function() {
 
 	describe( 'isCompressibleFile', function() {
 
-		it( 'should compress JS files', function() {
-			var isCompressible = isCompressibleHelper( 'file.js' );
-			expect( isCompressible ).to.equal( true );
-		} );
+		compressibleFiles.forEach( function( filename ) {
 
-		it( 'should compress CSS files', function() {
-			var isCompressible = isCompressibleHelper( 'file.css' );
-			expect( isCompressible ).to.equal( true );
-		} );
+			it( 'should compress ' + filename, function() {
+				var filePath = path.join( './test/support', filename );
+				var stream = fs.createReadStream( filePath );
+				var isCompressible = isCompressibleFile( stream );
+				expect( isCompressible ).to.equal( true );
+			} );
 
-		it( 'should compress HTML files', function() {
-			var isCompressible = isCompressibleHelper( 'file.html' );
-			expect( isCompressible ).to.equal( true );
-		} );
-
-		it( 'should be case insensitive', function() {
-			var isCompressible = isCompressibleHelper( 'uppercase.HTML' );
-			expect( isCompressible ).to.equal( true );
 		} );
 
 		it( 'should not compress GIF files', function() {
-			var isCompressible = isCompressibleHelper( 'file.gif' );
+			var stream = fs.createReadStream( './test/support/file.gif' );
+			var isCompressible = isCompressibleFile( stream );
 			expect( isCompressible ).to.equal( false );
 		} );
 
