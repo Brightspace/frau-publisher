3307d2e655885c0c4f9b657446d1814db3ef15e8
Carl Pacey <carl.pacey@gmail.com>
2014-12-22 10:19:39 -0500

    Switching from SandboxedModule to proxyquire
    The usage model is the same, but there's fewer hassles.

--- test/support/node.js
+++ test/support/node.js
@@ -2,12 +2,9 @@
 
 var chai = require('chai');
 var expect = chai.expect;
-var SandboxedModule = require('sandboxed-module');
-SandboxedModule.registerBuiltInSourceTransformer('istanbul');
 
 chai.should();
 chai.use(require('sinon-chai'));
 
 global.sinon = require('sinon');
 global.expect = chai.expect;
-global.SandboxedModule = SandboxedModule;
