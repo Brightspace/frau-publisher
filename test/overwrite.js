8ef562632e18c54ea028983c7c6c28a681e79b82
Carl Pacey <Carl.Pacey@d2l.com>
2014-12-23 10:34:24 -0500

    Converting overwrite to only check s3 once
    Also refactoring many of the tests.

--- test/overwrite.js
+++ test/overwrite.js
@@ -2,93 +2,155 @@
 
 var es = require('event-stream'),
 	gulp = require('gulp'),
-	mox = require('./mock-knox'),
-	optionsValidator = require('../src/optionsValidator'),
 	proxyquire = require('proxyquire');
-
-function getOptions( key ) {
-	return optionsValidator( {
-		id: 'some-ID',
-		initialPath: 'apps/',
-		devTag: 'some-tag',
-		creds: {
-			key: key,
-			secret: 'secret'
-		}
-	} );
-}
 
 describe( 'overwrite', function() {
 
-	var dataHandler, overwrite;
+	var overwrite,
+		createClient,
+		optsValidator,
+		client;
 
-	beforeEach( function () {
-		dataHandler = sinon.spy();
-		overwrite = proxyquire('../src/overwrite', {
-			knox: mox
-		} );
-		sinon.spy( mox, 'createClient' );
-	} );
+	it( 'should succeed when folder is empty', function( done ) {
+		setUpEmptyFolder();
 
-	afterEach( function() {
-		mox.createClient.restore();
-	} );
-
-	it( 'should succeed when bucket is empty', function( done ) {
 		gulp.src('./test/support/file.js')
-			.pipe( overwrite( getOptions('some-key') ) )
-			.on( 'data', dataHandler )
-			.on( 'end', function( err ) {
-				expect( dataHandler ).to.be.called;
-				done();
+			.pipe( overwrite( optsValidator ) )
+			.on( 'data', function( data ) {
+				try {
+					expect( data )
+						.to.have.deep.property('history[0]')
+						.that.contain('file.js');
+
+					done();
+				} catch (e) {
+					done(e);
+				}
 			} );
 	} );
 
 	it( 'should error when files exist in bucket', function( done ) {
-		var opts = getOptions('key-a');
+		setUpNonEmptyFolder();
+
+		optsValidator.getUploadPath.returns('some-folder');
+		var expectedErrorMsg = 'No files transferred because files already exists in some-folder';
+
 		gulp.src('./test/support/file.js')
-			.pipe( overwrite( opts ) )
-			.on( 'data', dataHandler )
-			.on( 'error', function( err ) {
-				var expectedErrorMsg = overwrite._getErrorMsg( opts );
-				expect( dataHandler ).to.not.be.called;
-				expect( err ).to.eql(new Error( expectedErrorMsg ) );
-				done();
+			.pipe( overwrite( optsValidator ) )
+			.on( 'data', function() {
+				done('should be no data');
+			} ).on( 'error', function( err ) {
+				try {
+					expect(err).to.deep.equal(new Error(expectedErrorMsg));
+
+					done();
+				} catch (e) {
+					done(e);
+				}
 			} );
 	} );
 
-	it( 'should handle data.Code error from knox', function( done ) {
+	it( 'should handle bad data.Code error from knox', function( done ) {
+		setUpList(null, { Code: 'some-code', Message: 'some-message' } );
+
 		gulp.src('./test/support/file.js')
-			.pipe( overwrite( getOptions('wrong-key') ) )
-			.on( 'data', dataHandler )
-			.on( 'error', function( err ) {
-				expect( dataHandler ).to.not.be.called;
-				done();
+			.pipe( overwrite( optsValidator ) )
+			.on( 'data', function() {
+				done('should be no data');
+			}).on( 'error', function( err ) {
+				try {
+					expect(err).to.deep.equal(new Error('some-message'));
+
+					done();
+				} catch (e) {
+					done(e);
+				}
 			} );
 	} );
 
-	it( 'should ignore a non-buffer file', function( done ) {
+	it( 'should handle err from knox', function( done ) {
+		var error = new Error('some-message');
+		setUpList(error);
+
 		gulp.src('./test/support/file.js')
-			.pipe( es.mapSync( function( file ) {
-				file.isBuffer = function() { return false; };
-				return file;
-			} ) )
-			.pipe( overwrite( getOptions('some-key') ) )
-			.on( 'data', dataHandler )
-			.on( 'end', function( err ) {
-				expect( dataHandler ).to.not.be.called;
-				done();
+			.pipe( overwrite( optsValidator ) )
+			.on( 'data', function() {
+				done('should be no data');
+			}).on( 'error', function( err ) {
+				try {
+					expect(err).to.equal(error);
+
+					done();
+				} catch (e) {
+					done(e);
+				}
 			} );
 	} );
 
 	it( 'should only get knox client once for multiple files', function( done ) {
-		gulp.src(['./test/support/file.js','./test/support/file.html'])
-			.pipe( overwrite( getOptions('some-key') ) )
-			.on( 'end', function() {
-				expect( mox.createClient ).to.have.been.calledOnce;
-				done();
-			} );
+		setUpEmptyFolder();
 
+		gulp.src(['./test/support/file.js','./test/support/file.html'])
+			.pipe( overwrite( optsValidator ) )
+			.on( 'end', function() {
+				try {
+					expect( createClient ).to.have.been.calledOnce;
+
+					done();
+				} catch (e) {
+					done(e);
+				}
+			} );
 	} );
 
+	it( 'should only call knox#list once for multiple files', function( done ) {
+		setUpEmptyFolder();
+
+		gulp.src(['./test/support/file.js','./test/support/file.html'])
+			.pipe( overwrite( optsValidator ) )
+			.on( 'end', function() {
+				try {
+					expect( client.list ).to.have.been.calledOnce;
+
+					done();
+				} catch (e) {
+					done(e);
+				}
+			} );
+	} );
+
+	beforeEach( function () {
+		client = {
+			list: function() {}
+		};
+
+		createClient = sinon.spy(function() {
+			return client;
+		});
+
+		overwrite = proxyquire('../src/overwrite', {
+			knox: {
+				createClient: createClient
+			}
+		} );
+
+		optsValidator = {
+			getCreds: sinon.stub().returns({}),
+			getUploadPath: sinon.stub().returns({})
+		};
+	} );
+
+	function setUpList(err, data) {
+		sinon.stub(client, 'list', function(object, cb) {
+			cb(err, data);
+		});
+	}
+
+	function setUpEmptyFolder() {
+		setUpList(null, { Contents: [] });
+	}
+
+	function setUpNonEmptyFolder() {
+		setUpList(null, { Contents: [ 'file.js'] });
+	}
 } );
