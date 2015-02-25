ce439fda21aaf5b1e04cc517d21ae7132260205c
Matt Jones <matt.jones@D2L.com>
2015-02-25 10:59:27 -0500

    Use single-quote instead of double-quote

--- test/optionsValidator.js
+++ test/optionsValidator.js
@@ -111,14 +111,14 @@ describe( 'options validator', function() {
 
         it( 'should set bucket to production cdn when none provided', function() {
             var options = optionsValidator(
-                { id: "id", devTag: "devTag", creds: { key: "key", secret: "mySecret" } }
+                { id: 'id', devTag: 'devTag', creds: { key: 'key', secret: 'mySecret' } }
             );
             expect(options.getCreds()).to.have.property('bucket', 'd2lprodcdn');
         } );
 
         it( 'should set bucket to provided bucket when one provided', function() {
             var options = optionsValidator(
-                { id: "id", devTag: "devTag", creds: { key: "key", secret: "mySecret", bucket: "test-bucket" } }
+                { id: 'id', devTag: 'devTag', creds: { key: 'key', secret: 'mySecret', bucket: 'test-bucket' } }
             );
             expect( options.getCreds() ).to.have.property('bucket', 'test-bucket');
         } );
@@ -134,14 +134,14 @@ describe( 'options validator', function() {
 	describe( 'getBaseLocation', function() {
         it( 'should return the production CDN when no bucket provided', function() {
             var options = optionsValidator(
-                { id: "id", devTag: "devTag", creds: { key: "key", secret: "mySecret" } }
+                { id: 'id', devTag: 'devTag', creds: { key: 'key', secret: 'mySecret' } }
             );
             expect( options.getBaseLocation() ).to.equal('https://s.brightspace.com/');
         } );
 
         it( 'should return the AWS bucket location when a bucket is provided', function() {
             var options = optionsValidator(
-                { id: "id", devTag: "devTag", creds: { key: "key", secret: "mySecret", bucket: "test-bucket" } }
+                { id: 'id', devTag: 'devTag', creds: { key: 'key', secret: 'mySecret', bucket: 'test-bucket' } }
             );
             expect( options.getBaseLocation() ).to.equal('https://s3.amazonaws.com/test-bucket/');
         } );
