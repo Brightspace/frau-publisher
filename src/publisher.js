de724b9f6019eeddfdf308ba1fb2b168569714ca
Jacob Parker <Jacob.Parker@D2L.com>
2016-05-30 22:01:04 +0100

    Publish additional app configs for {major} and {major}.{minor} releases

--- src/publisher.js
+++ src/publisher.js
@@ -2,10 +2,16 @@
 
 var compressor = require('./compressor'),
 	es = require('event-stream'),
+	filter = require('gulp-filter'),
+	mirror = require('gulp-mirror'),
 	optionsValidator = require('./optionsValidator'),
 	optionsProvider = require('./optionsProvider'),
 	overwrite = require('./overwrite'),
-	s3 = require('gulp-s3');
+	pipe = require('multipipe'),
+	rename = require('gulp-rename'),
+	s3 = require('gulp-s3'),
+	semver = require('semver'),
+	util = require('gulp-util');
 
 function helper( opts, initialPath ) {
 	opts.initialPath = initialPath;
@@ -26,18 +32,61 @@ function helper( opts, initialPath ) {
 
 			var duplexStream = es.duplex( overwriteCheck, gulpS3 );
 
-			// pipe overwrite -> compressor -> S3
-			overwriteCheck.pipe( compressorStream )
-				.pipe( gulpS3 );
+			overwriteCheck
+				.pipe( compressorStream )
+				.pipe( mirror(
+					uploadBundle( options ),
+					uploadSemverAppconfig( options )
+				) );
 
 			return duplexStream;
 
 		},
+
 		getLocation: function() {
 			var options = optionsValidator( opts );
 			return 'https://s.brightspace.com/' + options.getUploadPath();
 		}
 	};
+}
+
+function uploadBundle( options ) {
+	var s3Options = {
+		headers: {
+			'cache-control': 'public, max-age=31536000'
+		},
+		uploadPath: options.getUploadPath()
+	};
+
+	var gulpS3 = s3( options.getCreds(), s3Options );
+
+	return gulpS3;
+}
+
+function uploadSemverAppconfig( options ) {
+	var version = options.getVersion();
+
+	if ( !semver.valid(version) ) {
+		return util.noop();
+	}
+
+	var s3Options = {
+		headers: {
+			'cache-control': 'public, max-age=600'
+		},
+		uploadPath: options.getBasePath()
+	};
+
+	var gulpS3 = s3( options.getCreds(), s3Options );
+
+	return pipe(
+		filter(f => f.path.endsWith('appconfig.json.gz')),
+		mirror(
+			rename('appconfig.v' + semver.major( version ) + '.json.gz'),
+			rename('appconfig.v' + semver.major( version ) + '.' + semver.minor( version ) + '.json.gz')
+		),
+		gulpS3
+	);
 }
 
 module.exports = {
