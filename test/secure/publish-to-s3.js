ee6e0e028871c2e5a60791260358a7be44466314
Owen Smith <owen@omsmith.ca>
2018-10-01 19:08:42 -0400

    realer-world integration test

--- test/secure/publish-to-s3.js
+++ test/secure/publish-to-s3.js
@@ -1,32 +1,17 @@
 'use strict';
 
+var fs = require('fs');
+
 var frauPublisher = require('../../src/publisher'),
 	request = require('request'),
-	eventStream = require('event-stream'),
-	gulpUtil = require('gulp-util');
-
-function makeFile(filename, content) {
-	return new gulpUtil.File({
-		cwd: '/',
-		base: '/',
-		path: '/' + filename,
-		contents: new Buffer(content),
-		stat: {
-			size: Buffer.byteLength(content)
-		}
-	});
-}
+	vfs = require('vinyl-fs');
 
 describe('publisher', function() {
 	it('should publish new file', function(cb) {
 		var publisher = createPublisher(Math.random().toString(16).slice(2));
 
-		var html = makeFile('test.html', '<body></body>');
-		var svg = makeFile('test.svg', '<g></g>');
-		var woff = makeFile('test.woff', new Buffer(10).fill(0));
-
-		eventStream
-			.readArray([html, svg, woff])
+		vfs
+			.src('./test/test-files/*')
 			.pipe(publisher.getStream())
 			.on('end', function() {
 				Promise.all([
@@ -34,7 +19,7 @@ describe('publisher', function() {
 						request.get(publisher.getLocation() + 'test.html', { gzip: true }, function(err, res, body) {
 							if (err) return reject(err);
 							if (res.statusCode !== 200) return reject(new Error(res.statusCode));
-							if (body !== '<body></body>') return reject(new Error(body));
+							if (body !== fs.readFileSync('./test/test-files/test.html', 'utf8')) return reject(new Error(body));
 
 							if (res.headers['content-encoding'] !== 'gzip') return reject(new Error(res.headers['content-encoding']));
 							if (res.headers['content-type'] !== 'text/html; charset=utf-8') return reject(new Error(res.headers['content-type']));
@@ -46,7 +31,7 @@ describe('publisher', function() {
 						request.get(publisher.getLocation() + 'test.svg', { gzip: true }, function(err, res, body) {
 							if (err) return reject(err);
 							if (res.statusCode !== 200) return reject(new Error(res.statusCode));
-							if (body !== '<g></g>') return reject(new Error(body));
+							if (body !== fs.readFileSync('./test/test-files/test.svg', 'utf8')) return reject(new Error(body));
 
 							if (res.headers['content-encoding'] !== 'gzip') return reject(new Error(res.headers['content-encoding']));
 							if (res.headers['content-type'] !== 'image/svg+xml') return reject(new Error(res.headers['content-type']));
@@ -58,7 +43,7 @@ describe('publisher', function() {
 						request.get(publisher.getLocation() + 'test.woff', { gzip: true }, function(err, res, body) {
 							if (err) return reject(err);
 							if (res.statusCode !== 200) return reject(new Error(res.statusCode));
-							if (body !== woff.contents.toString()) return reject(new Error(body));
+							if (body !== fs.readFileSync('./test/test-files/test.woff', 'utf8')) return reject(new Error(body));
 
 							if (res.headers['content-encoding']) return reject(new Error(res.headers['content-encoding']));
 							if (res.headers['content-type'] !== 'application/font-woff') return reject(new Error(res.headers['content-type']));
@@ -77,7 +62,8 @@ describe('publisher', function() {
 		//  start that way because it seems unnecessarily wasteful.
 		var publisher = createPublisher('overwrite-test');
 
-		eventStream.readArray([makeFile('test.txt', 'some data')])
+		vfs
+			.src('./test/test-files/test.txt')
 			.pipe(publisher.getStream())
 			.on('error', function() {
 				cb();
