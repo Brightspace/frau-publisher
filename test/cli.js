1e52b9e51e7f413f82dc7fb570a1960657691eff
Owen Smith <owen@omsmith.ca>
2018-10-02 10:31:10 -0400

    test: add tests that exercise the cli

---
+++ test/cli.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var child_process = require('child_process');
+
+describe('cli', function() {
+	['app', 'lib'].forEach(function(moduleType) {
+		it('should fail with exit code 1 when an error occurs', function(done) {
+			var p = child_process.execFile('./bin/publishercli', [
+				'-m', moduleType,
+				'-t', '/frau-publisher-failing-test',
+				'-f', './test/test-files/*',
+				'-k', 'BAD_KEY',
+				'-s', 'BAD_SECRET',
+				'--devtag', 'fail'
+			])
+				.on('error', done)
+				.on('exit', function(code) {
+					if (code !== 1) {
+						return done(new Error('Expected exit code of 1, saw ' + code));
+					}
+
+					done();
+				});
+
+			p.stdout.pipe(process.stderr);
+			p.stderr.pipe(process.stderr);
+		});
+	});
+});
