45e0e0d1ccd7e72b635461e8d890a89a81e126d9
Owen Smith <owen@omsmith.ca>
2018-10-01 20:20:51 -0400

    make file paths work again for gulp3 users

--- src/publisher.js
+++ src/publisher.js
@@ -29,6 +29,10 @@ function helper(opts, initialPath) {
 			var otherStream = getOtherStream();
 
 			var splitter = es.map(function(file, cb) {
+				if (file.base[file.base.length - 1] === '/') {
+					file.base = file.base.substring(0, file.base.length - 1);
+				}
+
 				if (path.extname(file.path).toLowerCase() === '.html') {
 					htmlStream.write(file);
 					return cb(null, file);
