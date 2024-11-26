a73e92b4487495b2e5e060e82ea068314b541db8
GZolla <43836485+GZolla@users.noreply.github.com>
2024-11-26 11:55:17 -0800

    fix: Use new shared configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,9 @@
-import { fileURLToPath } from 'node:url';
-import { FlatCompat } from '@eslint/eslintrc';
 import globals from 'globals';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
-import path from 'node:path';
+import { nodeConfig } from 'eslint-config-brightspace'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
-const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
-	includeIgnoreFile(gitignorePath),
-	...compat.extends('brightspace/node-config'),
+	...nodeConfig,
 	{
 		languageOptions: {
 			sourceType: 'commonjs'
