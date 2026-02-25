a04d7a3ebeeccc0d1dd27556424433aca80e4c89
Dave Lockhart <dlockhart@users.noreply.github.com>
2026-02-25 13:29:49 -0500

    fix: upgrade dependencies (#242) (GAUD-9521)

--- test/support/node.js
+++ test/support/node.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai').default;
 
 chai.should();
-chai.use(require('sinon-chai'));
+chai.use(sinonChai);
 
-global.sinon = require('sinon');
+global.sinon = sinon;
 global.expect = chai.expect;
